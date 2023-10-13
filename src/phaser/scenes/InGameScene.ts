de5cb43e8a7c11b0103871d005776c021f136a9d
jungboseok <boseokjung@gmail.com>
2023-10-14 03:19:13 +0900

    feat: bunker sprite, healthBar

--- src/phaser/scenes/InGameScene.ts
+++ src/phaser/scenes/InGameScene.ts
@@ -1,20 +1,43 @@
-import { createTitleText } from "@/phaser/phaserUtils/titleText";
+// import { createTitleText } from "@/phaser/phaserUtils/titleText";
+import { HealthBar, HealthBarConfig } from "@/phaser/ui/HealthBar";
 import { Button } from "@/phaser/ui/upgrade/Button";
 
+const uiHeight = 200;
 export class InGameScene extends Phaser.Scene {
+  healthBar: HealthBar;
   constructor() {
     super("InGameScene");
   }
-  preload() {}
+  preload() {
+    this.load.image("bunker", "assets/bunker_100x100.png");
+    this.load.spritesheet("pixel_animals", "assets/pixel_animals.png", {
+      frameWidth: 16,
+      frameHeight: 16,
+    });
+  }
   create() {
-    createTitleText(this, "Select Level", 100);
+    // createTitleText(this, "Select Level", 100);
     this.createUI(this);
+    this.createBunker(this);
+  }
+  createBunker(scene: Phaser.Scene) {
+    const bunkerSprite = scene.add.sprite(0, 0, "bunker");
+    this.healthBar = new HealthBar(
+      scene,
+      bunkerSprite.x - HealthBarConfig.width / 2,
+      bunkerSprite.y - 40,
+      100
+    );
+    scene.add.container(
+      Number(scene.game.config.width) / 2,
+      Number(scene.game.config.height) / 2 - uiHeight / 2,
+      [bunkerSprite, this.healthBar.bar]
+    );
   }
   createUI(scene: Phaser.Scene) {
-    const height = 200;
     const uiContainer = scene.add.container(
       0,
-      Number(scene.game.config.height) - height
+      Number(scene.game.config.height) - uiHeight
     );
     const uiWrap = scene.add
       .rectangle(
@@ -22,7 +45,7 @@ export class InGameScene extends Phaser.Scene {
         0,
         // Number(scene.game.config.height) - height,
         Number(scene.game.config.width),
-        height
+        uiHeight
       )
       .setOrigin(0, 0)
       .setDepth(100)
@@ -35,16 +58,37 @@ export class InGameScene extends Phaser.Scene {
       { id: "attackSpeed", desc: "increase attack speed 1%" },
       { id: "income", desc: "increase income +0.5%" },
       { id: "upgradeBunker", desc: "upgrade bunker" },
-    ].map(({ desc }, index) => {
-      return new Button(scene, {
+    ].map(({ id, desc }, index) => {
+      const button = new Button(scene, {
         x: Number(scene.game.config.width) - 50 * (index + 1),
         y: 0,
         width: 50,
         height: 50,
         hoverText: desc,
-        onClick: () => {},
+        onClick: () => {
+          console.log("click", this, button, id);
+          this.events.emit("upgrade", id);
+        },
       });
+      return button;
     });
     uiContainer.add([uiWrap, ...buttons]);
+  }
+  createMap(scene: Phaser.Scene, mapKey: string) {
+    const map = scene.make.tilemap({
+      key: mapKey,
+    });
+    const jew_pastel_lineTiles = map.addTilesetImage(
+      "jew_pastel_line",
+      "jew_pastel_line"
+    );
+    const collision_layer = map.createLayer("bg_collision", [
+      jew_pastel_lineTiles,
+    ]);
+    collision_layer.setCollisionByExclusion([-1]);
+
+    const bunkerSpawnPoints = map.findObject("BunkerSpawn", ({ name }) => {
+      return name.includes("BunkerSpawn");
+    });
   }
 }
--- src/phaser/ui/upgrade/Button.ts
+++ src/phaser/ui/upgrade/Button.ts
@@ -6,22 +6,60 @@ export class Button extends Phaser.GameObjects.Container {
     { x, y, width, height, hoverText, onClick }
   ) {
     super(scene, x, y);
-    const textWrap = new Phaser.GameObjects.Rectangle(scene, 0, 0, 100, 30)
+    const textPadding = 20;
+    const buttonText = new Phaser.GameObjects.Text(
+      scene,
+      textPadding / 2,
+      textPadding / 2 / 2,
+      hoverText,
+      {
+        fontSize: "12px",
+        color: "#ffffff",
+        align: "center",
+      }
+    ).setOrigin(0);
+    const textWrap = new Phaser.GameObjects.Rectangle(
+      scene,
+      0,
+      0,
+      buttonText.displayWidth + textPadding,
+      buttonText.displayHeight + textPadding / 2
+    )
       .setFillStyle(0x0000ff, 0.5)
-      .setOrigin(0, 0);
-    const buttonText = new Phaser.GameObjects.Text(scene, 5, 5, hoverText, {
-      fontSize: "12px",
-      color: "#ffffff",
-    }).setOrigin(0, 0);
+      .setOrigin(0);
     const textContainer = scene.add
-      .container(-50, -50, [textWrap, buttonText])
+      .container(-150, -50, [textWrap, buttonText])
       .setVisible(false);
+
+    // const sprite = scene.add.sprite(100, 100, 'village').setInteractive()
+    // sprite.setOrigin(0, 0).setScale(0.1)
+
+    // sprite.on('pointerover', () => {
+    //     // Some options, you could add position, stylings etc
+    //     const tooltip = { text: 'This is a nice village sdfsdfasdfasdfasdfasdf' }
+
+    //     const tooltipX = sprite.getRightCenter().x - 20
+    //     const tooltipY = sprite.y - 20
+    //     const textPadding = 20
+
+    //     const text = this.add.text(textPadding, textPadding, tooltip.text, { color: '#000' })
+    //     const background = this.add.rectangle(0, 0, text.displayWidth + (textPadding * 2), text.displayHeight + (textPadding * 2), 0xffffff).setOrigin(0, 0)
+    //     this.tooltipContainer = this.add.container(tooltipX, tooltipY)
+    //     this.tooltipContainer.add(background)
+    //     this.tooltipContainer.add(text)
+    // })
 
     const button = new Phaser.GameObjects.Rectangle(scene, 0, 0, width, height)
       .setStrokeStyle(2, 0x0000ff, 1)
       .setOrigin(0, 0)
       .setInteractive()
-      .on("pointerdown", onClick)
+      .on("pointerdown", () => {
+        button.setAlpha(0.2);
+        onClick();
+      })
+      .on("pointerup", () => {
+        button.setAlpha(1);
+      })
       .on("pointerover", () => {
         textContainer.setVisible(true);
       })

4c8f4a13d0a7bf8c969c31fb54fc995ceab7d89d
jungboseok <boseokjung@gmail.com>
2023-10-13 17:45:57 +0900

    feat: ui buttons with tooltip

--- src/phaser/scenes/InGameScene.ts
+++ src/phaser/scenes/InGameScene.ts
@@ -1,4 +1,5 @@
 import { createTitleText } from "@/phaser/phaserUtils/titleText";
+import { Button } from "@/phaser/ui/upgrade/Button";
 
 export class InGameScene extends Phaser.Scene {
   constructor() {
@@ -11,10 +12,15 @@ export class InGameScene extends Phaser.Scene {
   }
   createUI(scene: Phaser.Scene) {
     const height = 200;
+    const uiContainer = scene.add.container(
+      0,
+      Number(scene.game.config.height) - height
+    );
     const uiWrap = scene.add
       .rectangle(
         0,
-        Number(scene.game.config.height) - height,
+        0,
+        // Number(scene.game.config.height) - height,
         Number(scene.game.config.width),
         height
       )
@@ -23,6 +29,22 @@ export class InGameScene extends Phaser.Scene {
       .setScrollFactor(0)
       .setFillStyle(0x00ff00, 0.2);
     // const button = new SelectLevelButton(scene, 100, 100, 1);
-    // const uiContainer = scene.add.container(0, 0).add(uiWrap);
+    const buttons = [
+      { id: "addSoldier", desc: "add new random attacker +1" },
+      { id: "attackDamage", desc: "increase attack damage 1%" },
+      { id: "attackSpeed", desc: "increase attack speed 1%" },
+      { id: "income", desc: "increase income +0.5%" },
+      { id: "upgradeBunker", desc: "upgrade bunker" },
+    ].map(({ desc }, index) => {
+      return new Button(scene, {
+        x: Number(scene.game.config.width) - 50 * (index + 1),
+        y: 0,
+        width: 50,
+        height: 50,
+        hoverText: desc,
+        onClick: () => {},
+      });
+    });
+    uiContainer.add([uiWrap, ...buttons]);
   }
 }
--- src/phaser/ui/upgrade/Button.ts
+++ src/phaser/ui/upgrade/Button.ts
@@ -1,25 +1,35 @@
 import { getUIStyle } from "@/phaser/constants";
 
 export class Button extends Phaser.GameObjects.Container {
-  buttonText: Phaser.GameObjects.Text;
-
-  constructor(scene: Phaser.Scene, { x, y, text, onClick }) {
+  constructor(
+    scene: Phaser.Scene,
+    { x, y, width, height, hoverText, onClick }
+  ) {
     super(scene, x, y);
+    const textWrap = new Phaser.GameObjects.Rectangle(scene, 0, 0, 100, 30)
+      .setFillStyle(0x0000ff, 0.5)
+      .setOrigin(0, 0);
+    const buttonText = new Phaser.GameObjects.Text(scene, 5, 5, hoverText, {
+      fontSize: "12px",
+      color: "#ffffff",
+    }).setOrigin(0, 0);
+    const textContainer = scene.add
+      .container(-50, -50, [textWrap, buttonText])
+      .setVisible(false);
 
-    const button = new Phaser.GameObjects.Rectangle(scene, 0, 0, 100, 100);
-    button.setStrokeStyle(2, 0x0000ff, 1);
-    this.buttonText = new Phaser.GameObjects.Text(
-      scene,
-      0,
-      0,
-      text,
-      getUIStyle()
-    );
-    button.setInteractive();
-    button.on("pointerdown", onClick);
-    this.add(button);
-    this.add(this.buttonText);
+    const button = new Phaser.GameObjects.Rectangle(scene, 0, 0, width, height)
+      .setStrokeStyle(2, 0x0000ff, 1)
+      .setOrigin(0, 0)
+      .setInteractive()
+      .on("pointerdown", onClick)
+      .on("pointerover", () => {
+        textContainer.setVisible(true);
+      })
+      .on("pointerout", () => {
+        textContainer.setVisible(false);
+      });
 
+    this.add([button, textContainer]);
     scene.add.existing(this);
   }
 }

dbe2a0a13f78d7515c5357c8a9d1964617b6f768
jungboseok <boseokjung@gmail.com>
2024-03-04 15:16:47 +0900

    fix: phase data 정리

--- src/phaser/constants/phase.ts
+++ src/phaser/constants/phase.ts
@@ -1,6 +1,8 @@
+const TOTAL_CHAR = 24;
 export const getPhaseData = () =>
   Array.from({ length: 100 }, (_, index) => {
-    const frameNo = index > 20 ? ((index % 20) + 1) * 2 : (index + 1) * 2;
+    const frameNo =
+      index >= TOTAL_CHAR ? ((index % TOTAL_CHAR) + 1) * 2 : (index + 1) * 2;
     return {
       phase: index + 1,
       count: 30,
